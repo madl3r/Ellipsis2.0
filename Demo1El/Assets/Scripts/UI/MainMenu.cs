5d2ee309752469006ec17b6bc663fdca35b4bf6c
auto_puller <Xelaadryth@users.noreply.github.com>
2018-06-24 12:58:50 -0700

    Remaking the main menu into the scene

---
+++ Demo1El/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject mainMenuObject;
+	public Button playButton, optionsButton, quitButton;
+
+	public GameObject optionsMenuObject;
+	public Button musicButton, soundFXButton, backButton;
+
+	// Use this for initialization
+	void Start () {
+		playButton.onClick.AddListener(Play);
+		optionsButton.onClick.AddListener(Options);
+		quitButton.onClick.AddListener(Quit);
+		
+		musicButton.onClick.AddListener(Music);
+		soundFXButton.onClick.AddListener(SoundFX);
+		backButton.onClick.AddListener(Back);
+
+		playButton.Select();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Play()
+	{
+		Time.timeScale = 1.0f;
+		SceneManager.LoadScene("DemoScene1");
+	}
+
+	void Options()
+	{
+		mainMenuObject.SetActive(false);
+		optionsMenuObject.SetActive(true);
+
+		backButton.Select();
+	}
+
+	void Quit()
+	{
+		Application.Quit();
+		Debug.Log("Quit!");
+	}
+
+	void Music()
+	{
+		// TODO
+	}
+
+	void SoundFX()
+	{
+		// TODO
+	}
+
+	void Back()
+	{
+		optionsMenuObject.SetActive(false);
+		mainMenuObject.SetActive(true);
+
+		optionsButton.Select();
+	}
+}
